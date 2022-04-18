39d42130aeb9586b2760f589e38e6fd3cb2e016e
Nadellz <nada.amrouche@gmail.com>
2022-04-18 01:08:06 +0100

    commited3

--- docs/js/main.js
+++ docs/js/main.js
@@ -11,11 +11,7 @@
 
   // Preloader
   $(window).on('load', function () {
-    if ($('#preloader').length) {
-      $('#preloader').delay(0).fadeOut('slow', function () {
-        $(this).remove();
-      });
-    }
+    
   });
 
   // Back to top button
